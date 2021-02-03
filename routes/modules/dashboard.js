6ff96ae768ab5ed24d6ecb495c1bab0b97adcfb2
tyttseng <tyttseng@gmail.com>
2021-02-03 19:43:08 +0800

    add register page and router

--- routes/modules/dashboard.js
+++ routes/modules/dashboard.js
@@ -5,6 +5,7 @@ const users = require('../../users')
 router.get('/', (req, res) => {
   // 依據登入時在session中存取的userId找到該使用者，並顯示個人化訊息（在此為使用者姓名）
   const user = users.find(user => user.firstName === req.session.name)
+  // 計算dashboard訪問次數
   req.session.views ? req.session.views += 1 : req.session.views = 1
   return res.render('dashboard', { user: user.firstName, count: req.session.views })
 })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,6 +6,10 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 router.post('/login', (req, res) => {
   const { email, password } = req.body
   // 尋找帳號密碼皆相同的使用者
@@ -16,6 +20,22 @@ router.post('/login', (req, res) => {
   } else { // 若使用者存在，則在session中存取user.id供後續使用，並前往個人化頁面
     req.session.name = user.firstName
     console.log(req.session)
+    res.redirect('/dashboard')
+  }
+})
+
+router.post('/register', (req, res) => {
+  const { username, email, password } = req.body
+  const exist = users.find(user => user.email === email)
+  if (exist) {
+    res.render('register', { error: true })
+  } else {
+    users.push({
+      firstName: username,
+      email,
+      password
+    })
+    req.session.name = username
     res.redirect('/dashboard')
   }
 })
